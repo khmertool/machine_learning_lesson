5e5dea353ac0b307e65e7fc358f184de2743a5ca
khmertool <46778258+khmertool@users.noreply.github.com>
2019-05-25 11:51:56 +0700

    add file

---
+++ 1_hello_world.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 24 08:35:35 2019
+
+@author: danhhong
+"""
+from sklearn import tree
+
+#ប្រមូល និងរៀបចំទិន្នន័យ
+data = [[1,4,3,7],
+        [2,2,6,3],
+        [1,2,6,3],
+        [5,6,6,4],
+        [1,6,6,7],
+        [4,2,4,7],
+        [5,2,2,7]]
+
+label = [0,1,0,0,0,0,1]
+
+#training
+mytree = tree.DecisionTreeClassifier()
+train_data = mytree.fit(data,label)
+
+#ព្យាករណ៍លទ្ធផល
+input_data = [[1,6,4,3]]
+
+result = train_data.predict(input_data)
+
+print(result)
