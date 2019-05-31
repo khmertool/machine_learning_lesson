47dd016735e418264c557af6d627d08141b00586
khmertool <46778258+khmertool@users.noreply.github.com>
2019-05-31 18:49:42 +0700

    add
    add

---
+++ 3_matplotlib_practice_1.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+
+'''
+គូសបន្ទាត់សាមីកាដឺក្រេទី១ y = 2x - 1
+
+ x | y
+ ------
+ 0 | -1
+ 1 | 1
+ 2 | 3
+ 3 | 5
+'''
+x = [1, 2, 3]
+y = [1, 3, 5]
+
+plt.plot(x, y)
+plt.show()
