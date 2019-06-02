a3303d0526f7b2f0bb88e30304290ad3dc728986
khmertool <46778258+khmertool@users.noreply.github.com>
2019-06-02 22:00:26 +0700

    add file
    add

---
+++ 5_dataframe.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import numpy as np
+from sklearn.tree import DecisionTreeClassifier
+
+fields = ['sepal.length', 'sepal.width', 
+          'petal.length', 'petal.width', 
+          'variety']
+data_frame = pd.read_csv("data/iris.csv", 
+                         skipinitialspace=True, 
+                         usecols=fields)
+
+# បំបែក data frame ទៅជា data & label
+data = data_frame[['sepal.length', 'sepal.width', 
+                   'petal.length', 'petal.width']]
+label = data_frame[['variety']]
+
+data_array = np.asarray(data)   # បំលែងទៅ numpy array
+label_array = np.asarray(label) # បំលែងទៅ numpy array
+
+# បង្រៀនម៉ាស៊ីន
+model = DecisionTreeClassifier()
+my_model = model.fit(data_array, label_array)
+
+# ព្យាករណ៍
+x_new_iris = np.array([[1.7, 4.0, 5.3, 6.0]]) # ផ្កាថ្មី
+result = my_model.predict(x_new_iris)
+print(result)
